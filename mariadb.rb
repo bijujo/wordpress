76250eb622bac0f5924fd3ef63a22abd9c73e910
EC2 Default User <ec2-user@ip-10-89-146-7.ec2.internal>
2019-07-13 02:05:24 +0000

    add db creation step

--- mariadb.rb
+++ mariadb.rb
@@ -9,3 +9,12 @@ end
 service "mariadb" do
   action [:enable, :start]
 end
+# Set root password
+execute 'set_root_pw' do
+  command '/usr/bin/mysqladmin -u root password root123'
+end
+
+# Create wordpress DB
+execute 'create_wordpress_db' do
+  command '/usr/bin/mysqladmin -u root -proot123 create wordpress'
+end
